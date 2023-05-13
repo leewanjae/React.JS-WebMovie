8e2a68f0e9abe6144895dfb1cc4ff8237c388c4d
GGmj <dhkswo1@gmail.com>
2023-05-14 00:32:47 +0900

    Use useEffect to fire only when a keyword is updated

--- react-for-movie/src/App.js
+++ react-for-movie/src/App.js
@@ -2,13 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("CALL THE API only once");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword); // run when 'keyword' changes
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
