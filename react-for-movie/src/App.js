ed066e9a8a1f216237ddbc0deb555a1bc39637ee
GGmj <dhkswo1@gmail.com>
2023-05-15 20:09:03 +0900

    Coin Selector

--- react-for-movie/src/App.js
+++ react-for-movie/src/App.js
@@ -1,35 +1,30 @@
 import { useState, useEffect } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((prev) => [toDo, ...prev]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>기록 {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write yout to do..."
-        />
-        <button>Add To DO </button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((toDo, index) => (
-          <li key={index}>{toDo}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
